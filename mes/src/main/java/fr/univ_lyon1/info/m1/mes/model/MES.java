43a921dec5a4789c1bd19a5154783266306c95e5
Bonis Alexis <alexis.bonis@etu.univ-lyon1.fr>
2022-09-09 08:14:46 +0200

    Corrige les erreurs pour que le pipeline fonctionne
    finalisation pour opti erreurs checkstyles

--- mes/src/main/java/fr/univ_lyon1/info/m1/mes/model/MES.java
+++ mes/src/main/java/fr/univ_lyon1/info/m1/mes/model/MES.java
@@ -37,7 +37,7 @@ public class MES {
         final Patient a = createPatient("Alice Foo", "299010212345678");
         final Patient b = createPatient("Bob Bar", "199010212345678");
         createPatient("Charles Boz", "102020212345678");
-        final HealthProfessional z = new Masseur("Dr. Zemmour", this);
+        new Masseur("Dr. Zemmour", this);
         final HealthProfessional w = new HealthProfessional("Dr. Who", this);
         final HealthProfessional s = new Dentist("Dr. Strange", this);
         new Homeopath("Dr. Hahnemann", this);
